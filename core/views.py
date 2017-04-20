d43aba0edaa0e3d0c3dad4e735b379c0ad9c498c
Ekaterina <johndoe@example.com>
2017-04-21 00:47:47 +0300

    n

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from apps.certificate.models import Certificate
+from core.forms import TicketForm
+
+__author__ = 'alexey'
+
+
+def index(request):
+    form = TicketForm()
+    certificate_list = Certificate.objects.all()
+    return render(request, 'index.html', {
+        'form': form,
+        'certificate_list': certificate_list,
+    })
+#
